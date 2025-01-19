c9167888b921cf39005cd8a9d0a77dc17ffca930
Max Ferrigni <maxferrigni@gmail.com>
2025-01-19 12:47:58 -0800

    Create supabase.js

---
+++ config/supabase.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://kketfaaihwjflpdngtvo.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtrZXRmYWFpaHdqZmxwZG5ndHZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzczMTg1MTAsImV4cCI6MjA1Mjg5NDUxMH0.PYKJKO39GCAXm-3zDFnQbSy1-oc4iuE4neLnfItIC90';
+
+export { SUPABASE_URL, SUPABASE_KEY };
