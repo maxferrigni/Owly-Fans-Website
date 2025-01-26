be8c35bc6ebb5c80bbddc62f0693ee5745250562
Max Ferrigni <maxferrigni@gmail.com>
2025-01-25 20:16:45 -0800

    Update supabase.js

--- config/supabase.js
+++ config/supabase.js
@@ -1,4 +1,5 @@
 const SUPABASE_URL = 'https://kketfaaihwjflpdngtvo.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtrZXRmYWFpaHdqZmxwZG5ndHZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzczMTg1MTAsImV4cCI6MjA1Mjg5NDUxMH0.PYKJKO39GCAXm-3zDFnQbSy1-oc4iuE4neLnfItIC90';
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
-export { SUPABASE_URL, SUPABASE_KEY };
+export { supabase };
